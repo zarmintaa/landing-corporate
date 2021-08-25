2f34261a638ab86894aa092bff5bb0c86d24e998
zarminta <18102217@ittelkom-pwt.ac.id>
2021-08-25 14:29:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -77,5 +77,15 @@ console.log(logo.src);
 console.log(logo.getAttribute('src'));
 console.log(logo.className);
 
-// data attribute
+// Scrlloling
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+
+  section1.scrollIntoView({
+    behavior: 'smooth'
+  })
+});
