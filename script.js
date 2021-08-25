d428a91039415a22e076bfc2b5fb60b60413506f
zarminta <18102217@ittelkom-pwt.ac.id>
2021-08-25 13:48:46 +0700

    style,attr,data

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,13 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+})
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +35,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const allSection = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+console.log(allSection);
+console.log(allButtons);
+
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `We use cookies for improved functionality and analytics<button class='btn  btn--close-cookie'>Got it!</button>`;
+//
+// header.prepend(message);
+// header.append(message)
+// header.before(message)
+header.after(message)
+// header.append(message.cloneNode(true));
+
+document.querySelector('.btn--close-cookie')
+  .addEventListener('click', () => {
+    // message.remove()
+    message.parentElement.removeChild(message);
+  });
+
+// Styles
+
+message.style.backgroundColor = '#37383d'
+message.style.width = '120%';
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+console.log(logo.className);
+
+// data attribute
+
