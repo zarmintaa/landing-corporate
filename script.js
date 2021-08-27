12b6232064a560ea4b0f95197764e776c66005ba
zarminta <18102217@ittelkom-pwt.ac.id>
2021-08-27 13:22:00 +0700

    Update script.js
    Revealing elements

--- script.js
+++ script.js
@@ -173,16 +173,22 @@ headerObserver.observe(header);
 const allSection = document.querySelectorAll('.section');
 
 const revealSection = function(entries, observer) {
-
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 }
 
 const sectionObserver = new IntersectionObserver(
   revealSection, {
+    root: null,
+    threshold: 0.15
   }
 );
 
 allSection.forEach(function(section) {
   sectionObserver.observe(section);
+  section.classList.add('section--hidden');
 })
 
 
