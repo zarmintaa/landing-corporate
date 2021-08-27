e24e26b2cd89f32417cc2d2e4811cf70c99d3b38
zarminta <18102217@ittelkom-pwt.ac.id>
2021-08-27 13:07:41 +0700

    Update script.js
    add sticky navigation

--- script.js
+++ script.js
@@ -125,6 +125,49 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 
+// Sticky navbar
+
+// const initialCoords = section1.getBoundingClientRect()
+//
+// window.addEventListener('scroll', function(e) {
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky')
+//   else nav.classList.remove('sticky');
+// })
+
+
+// const obsCallback = function(entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   })
+// };
+//
+// const obsOption = {
+//   root: null,
+//   threshold: [0, 0.2]
+// };
+//
+// const observer = new IntersectionObserver(obsCallback, obsOption);
+// observer.observe(section1);
+
+
+const header = document.querySelector('.header');
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+
 /////////////////////////////////////
 /////////////////////////////////////
 /////////////////////////////////////
